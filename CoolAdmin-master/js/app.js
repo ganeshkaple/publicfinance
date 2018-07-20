e30938c6f872f4633ba182e4959a7ecb27df2f84
Ganesh Kaple <gvklive@gmail.com>
2018-07-20 16:54:11 +0530

    SOlved method not found error, Moved the

--- CoolAdmin-master/js/app.js
+++ CoolAdmin-master/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('products', []);
 
 function loginViaFirebase(user_name, password, $window) {
-	initFirebaseAuth();
+//	initFirebaseAuth();
 	firebase.auth().signInWithEmailAndPassword(user_name, password).then(function (result) {
 		alert('Login Successful, Redirecting now');
 		result.user.getIdToken();
@@ -20,118 +20,6 @@ function loginViaFirebase(user_name, password, $window) {
 	});
 }
 
-// Signs-out of Firebase.
-function signOut() {
-	// Sign out of Firebase.
-	firebase.auth().signOut();
-}
-
-// Initiate firebase auth.
-function initFirebaseAuth() {
-	// Listen to auth state changes.
-	firebase.auth().onAuthStateChanged(authStateObserver);
-}
-
-// Returns the signed-in user's profile Pic URL.
-function getProfilePicUrl() {
-	return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
-}
-
-// Returns the signed-in user's display name.
-function getUserName() {
-	return firebase.auth().currentUser.displayName;
-}
-
-// Returns true if a user is signed-in.
-function isUserSignedIn() {
-	return !!firebase.auth().currentUser;
-}
-
-// Requests permissions to show notifications.
-function requestNotificationsPermissions() {
-	console.log('Requesting notifications permission...');
-	firebase.messaging().requestPermission().then(function () {
-		// Notification permission granted.
-		saveMessagingDeviceToken();
-	}).catch(function (error) {
-		console.error('Unable to get permission to notify.', error);
-	});
-}
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-function authStateObserver(user) {
-	if (user) { // User is signed in!
-		// Get the signed-in user's profile pic and name.
-		var profilePicUrl = getProfilePicUrl();
-		var userName = getUserName();
-		var profilePic = $scope.profile_pic;
-		var userFullName = $scope.name;
-
-		// Set the user's profile pic and name.
-
-		profilePic.style.backgroundImage = 'url(' + profilePicUrl + ')';
-		userFullName.textContent = userName;
-
-		// Show user's profile and sign-out button.
-		userFullName.removeAttribute('hidden');
-		profilePic.removeAttribute('hidden');
-		signOutButton.removeAttribute('hidden');
-
-		// Hide sign-in button.
-		signOutButton.setAttribute('hidden', 'true');
-
-		// We save the Firebase Messaging Device token and enable notifications.
-		saveMessagingDeviceToken();
-	} else { // User is signed out!
-		// Hide user's profile and sign-out button.
-		userFullName.setAttribute('hidden', 'true');
-		profilePic.setAttribute('hidden', 'true');
-		signOutButton.setAttribute('hidden', 'true');
-
-		// Show sign-in button.
-		signInButton.removeAttribute('hidden');
-	}
-}
-
-// Saves the messaging device token to the datastore.
-function saveMessagingDeviceToken() {
-	firebase.messaging().getToken().then(function (currentToken) {
-		if (currentToken) {
-			console.log('Got FCM device token:', currentToken);
-			// Saving the Device Token to the datastore.
-			firebase.database().ref('/fcmTokens').child(currentToken)
-				.set(firebase.auth().currentUser.uid);
-		} else {
-			// Need to request permissions to show notifications.
-			requestNotificationsPermissions();
-		}
-	}).catch(function (error) {
-		console.error('Unable to get messaging token.', error);
-	});
-}
-
-
-// Returns true if user is signed-in. Otherwise false and displays a message.
-function checkSignedInWithMessage() {
-	// Return true if the user is signed in Firebase
-	if (isUserSignedIn()) {
-		return true;
-	}
-
-	// Display a message to the user using a Toast.
-	var data = {
-		message: 'You must sign-in first',
-		timeout: 2000
-	};
-	signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
-	return false;
-}
-
-// Resets the given MaterialTextField.
-function resetMaterialTextfield(element) {
-	element.value = '';
-	element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
-}
 
 
 app.controller('productCTRL', function ($scope, $http, $window) {
